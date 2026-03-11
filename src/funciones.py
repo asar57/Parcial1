2b932bd5a89a87e74b93b6e83380c5b91eb1d589
jose <jmbermudez81@ucatolica.edu.co>
2026-03-11 13:25:38 -0500

    mejora  devolver nota máxima con su posición

--- src/funciones.py
+++ src/funciones.py
@@ -1,5 +1,6 @@
 def nota_maxima(notas):
     if len(notas) == 0:
         return None
-    return max(notas)
-
+    max_nota = max(notas)
+    indice = notas.index(max_nota)
+    return max_nota, indice
