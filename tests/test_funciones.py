ce127da5db844cb97f377f195bd69da42d2c00dd
jose <jmbermudez81@ucatolica.edu.co>
2026-03-11 13:03:18 -0500

    test: agregar test para nota_maxima

---
+++ tests/test_funciones.py
@@ -0,0 +1,10 @@
+from src.funciones import nota_maxima
+
+def test_arreglo_vacio():
+    assert nota_maxima([]) == None
+
+def test_notas_normales():
+    assert nota_maxima([3,4,2,5]) == 5
+
+def test_notas_decimales():
+    assert nota_maxima([2.5,4.8,3.1]) == 4.8
